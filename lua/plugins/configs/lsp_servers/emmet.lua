2a3aa431b9d8d5463986b2b1333d82354e0975bf
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-11-27 13:59:01 -0300

    remove 'emmet_language_server'

--- lua/plugins/configs/lsp_servers/emmet.lua
+++ lua/plugins/configs/lsp_servers/emmet.lua
@@ -1,30 +1,8 @@
 local lspconfig = require "lspconfig"
-local configs = require "lspconfig/configs"
 
 local on_attach = require("plugins.configs.lsp_servers.on_attach")
 local capabilities = require("plugins.configs.lsp_servers.capabilities")
 
-if not lspconfig.emmet_language_server then
-    configs.emmet_language_server = {
-        default_config = {
-            cmd = {"emmet-language-server", "--stdio"},
-            filetypes = {
-                "html",
-                "typescriptreact",
-                "javascriptreact",
-                "javascript",
-                "typescript",
-                "javascript.jsx",
-                "typescript.tsx",
-                "css"
-            },
-            root_dir = function(fname)
-                return vim.loop.cwd()
-            end,
-            settings = {}
-        }
-    }
-end
 lspconfig.emmet_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,

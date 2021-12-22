053f93f4936a83ef891d26238efcc9bd6657f51f
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-22 17:42:29 -0300

    update: change lualine sections position

--- lua/plugins/configs/lualine/sections.lua
+++ lua/plugins/configs/lualine/sections.lua
@@ -30,112 +30,20 @@ M.a = {
 
 M.b = {
     {
-        "b:gitsigns_head",
-        icon = " ",
-        color = {
-            fg = colors.red,
-            gui = "bold"
-        },
-        cond = conditions.git,
-        padding = {
-            left = 0,
-            right = 0
-        }
-    },
-    {
-        "diff",
-        source = function()
-            local gitsigns = vim.b.gitsigns_status_dict
-            if gitsigns then
-                return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed
-                }
-            end
-        end,
-        colored = true,
-        symbols = {added = "  ", modified = " 柳", removed = "  "},
-        cond = conditions.width
-    }
-}
-
-M.c = {
-    {
-        "filetype",
-        icon_only = false, -- Display only icon for filetype
-        color = {
-            fg = colors.purple
-        },
-        cond = conditions.nvimtree
-    },
-    {
         "filename",
         file_status = true, -- displays file status (readonly status, modified status)
         path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-        -- shorting_target = 40, -- Shortens path to leave 40 space in the window
+        shorting_target = 40, -- Shortens path to leave 40 space in the window
         -- for other components. Terrible name any suggestions?
         symbols = {
             modified = " ﱣ",
-            readonly = " "
+            readonly = " ",
+            unnamed = "[No Name]"
         },
         color = {
             fg = colors.purple
         },
         cond = conditions.empty
-    }
-}
-
-M.x = {
-    {
-        "diagnostics",
-        sources = {"nvim_diagnostic"},
-        sections = {"error", "warn", "info", "hint"},
-        symbols = {
-            error = " ",
-            warn = " ",
-            info = " ",
-            hint = " "
-        },
-        cond = conditions.nvimtree
-    },
-    {
-        function()
-            local msg = "LSP Inactive"
-            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-            local clients = vim.lsp.get_active_clients()
-            if next(clients) == nil then
-                return msg
-            end
-            for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    return client.name
-                end
-            end
-            return msg
-        end,
-        icon = " LSP:",
-        cond = conditions.nvimtree,
-        color = {
-            fg = colors.gray_fg,
-            gui = "bold"
-        }
-    }
-}
-
-M.y = {
-    {
-        "o:encoding",
-        upper = true,
-        color = {
-            fg = colors.purple
-        },
-        padding = {
-            left = 0,
-            right = 1
-        },
-        cond = conditions.simple
     },
     {
         "location",
@@ -161,18 +69,110 @@ M.y = {
     }
 }
 
-M.z = {
+M.c = {
+    {
+        "diff",
+        source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+                return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed
+                }
+            end
+        end,
+        colored = true,
+        symbols = {added = "  ", modified = " 柳", removed = "  "},
+        cond = conditions.width
+    }
+}
+
+M.x = {
+    {
+        "diagnostics",
+        sources = {"nvim_diagnostic"},
+        sections = {"error", "warn", "info", "hint"},
+        symbols = {
+            error = " ",
+            warn = " ",
+            info = " ",
+            hint = " "
+        },
+        cond = conditions.nvimtree
+    },
+    {
+        function()
+            local msg = "inactive"
+            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+            local clients = vim.lsp.get_active_clients()
+            if next(clients) == nil then
+                return msg
+            end
+            for _, client in ipairs(clients) do
+                local filetypes = client.config.filetypes
+                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    return client.name
+                end
+            end
+            return msg
+        end,
+        icon = "",
+        cond = conditions.nvimtree,
+        color = {
+            fg = colors.vibrant_green,
+            gui = "bold"
+        }
+    }
+}
+
+M.y = {
     {
         "fileformat",
         symbols = {
-            unix = "",
-            dos = "",
-            mac = "" -- e711
+            unix = "LF",
+            dos = "CRLF",
+            mac = "CR"
         },
         cond = conditions.simple,
         padding = {
             left = 0,
             right = 1
+        }
+    },
+    {
+        "o:encoding",
+        fmt = string.upper,
+        color = {
+            fg = colors.purple
+        },
+        padding = {
+            left = 0,
+            right = 1
+        },
+        cond = conditions.simple
+    }
+}
+
+M.z = {
+    {
+        "filetype",
+        icon_only = false, -- Display only icon for filetype
+        color = {
+            fg = colors.purple
+        },
+        cond = conditions.nvimtree
+    },
+    {
+        "b:gitsigns_head",
+        icon = " ",
+        color = {
+            fg = colors.red,
+            gui = "bold"
+        },
+        cond = conditions.git,
+        padding = {
+            left = 0
         }
     },
     {
@@ -196,32 +196,13 @@ M.z = {
             end
             return format_file_size(file) .. " "
         end,
-        cond = conditions.simple,
+        condition = conditions.simple,
         color = {
             fg = colors.blue
         },
-        padding = {
-            left = 0,
-            right = 0
-        }
+        left_padding = 0,
+        right_padding = 1
     }
-    -- {
-    --     function()
-    --         local current_line = vim.fn.line "."
-    --         local total_lines = vim.fn.line "$"
-    --         local chars = {"_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"}
-    --         local line_ratio = current_line / total_lines
-    --         local index = math.ceil(line_ratio * #chars)
-    --         return chars[index]
-    --     end,
-    --     padding = {
-    --         right = 0
-    --     },
-    --     color = {
-    --         fg = colors.yellow
-    --     },
-    --     cond = conditions.nvimtree
-    -- }
 }
 
 return M

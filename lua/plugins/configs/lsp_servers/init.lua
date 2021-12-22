74c4ee23f3fe829d2df63aab65132b265a08c82e
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-22 13:42:06 -0300

    fix: remove jdtls plugin and keep jdtls language server

--- lua/plugins/configs/lsp_servers/init.lua
+++ lua/plugins/configs/lsp_servers/init.lua
@@ -3,6 +3,7 @@ require("plugins.configs.lsp_servers.vue")
 require("plugins.configs.lsp_servers.python")
 require("plugins.configs.lsp_servers.angular")
 require("plugins.configs.lsp_servers.tsserver")
+require("plugins.configs.lsp_servers.jsonls")
 require("plugins.configs.lsp_servers.deno")
 require("plugins.configs.lsp_servers.eslint")
 require("plugins.configs.lsp_servers.rust")
@@ -11,15 +12,6 @@ require("plugins.configs.lsp_servers.html")
 require("plugins.configs.lsp_servers.css")
 require("plugins.configs.lsp_servers.emmet")
 require("plugins.configs.lsp_servers.clangd")
--- require("plugins.configs.lsp_servers.java")
+require("plugins.configs.lsp_servers.java")
 require("plugins.configs.lsp_servers.graphql")
 require("plugins.configs.lsp_servers.sqlls")
-
-vim.api.nvim_exec(
-    [[
-  augroup lsp
-    au!
-    au FileType java lua require("plugins.configs.lsp_servers.java")
-  augroup end]],
-    false
-)

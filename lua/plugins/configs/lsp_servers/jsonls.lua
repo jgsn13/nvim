b6054bed646edcde5fb28f300dafb8e751b4181c
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-22 13:42:28 -0300

    add json language server

---
+++ lua/plugins/configs/lsp_servers/jsonls.lua
@@ -0,0 +1,10 @@
+local on_attach = require("plugins.configs.lsp_servers.on_attach")
+local capabilities = require("plugins.configs.lsp_servers.capabilities")
+
+require "lspconfig".jsonls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+        debounce_text_changes = 150
+    }
+}

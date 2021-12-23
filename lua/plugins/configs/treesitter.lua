b76d72415bccd60dfd7c3100412b6378bf365225
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-22 22:17:22 -0300

    chore: setup rainbow and autotag

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -12,5 +12,25 @@ ts_config.setup {
     highlight = {
         enable = true,
         use_languagetree = true
+    },
+    autotag = {
+        enable = true,
+        filetypes = {
+            "html",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "svelte",
+            "vue"
+        }
+    },
+    rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
     }
 }

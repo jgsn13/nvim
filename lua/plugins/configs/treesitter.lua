872d2c21e157777aa92d011f2da2f78f15e95bce
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-23 10:22:11 -0300

    chore: setup orgmode

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -3,15 +3,32 @@ if not present then
     return
 end
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+parser_config.org = {
+    install_info = {
+        url = "https://github.com/milisims/tree-sitter-org",
+        revision = "f110024d539e676f25b72b7c80b0fd43c34264ef",
+        files = {"src/parser.c", "src/scanner.cc"}
+    },
+    filetype = "org"
+}
+
 ts_config.setup {
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+        -- disable = {"org"}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+        additional_vim_regex_highlighting = {"org"} -- Required since TS highlighter doesn't support all syntax features (conceal)
+    },
     ensure_installed = {
         "lua",
         "typescript",
-        "javascript"
-    },
-    highlight = {
-        enable = true,
-        use_languagetree = true
+        "javascript",
+        "css",
+        "html",
+        "json",
+        "org"
     },
     autotag = {
         enable = true,
@@ -34,3 +51,10 @@ ts_config.setup {
         -- termcolors = {} -- table of colour name strings
     }
 }
+
+require("orgmode").setup(
+    {
+        org_agenda_files = {"~/Documents/org/*", "~/my-orgs/**/*"},
+        org_default_notes_file = "~/Documents/org/refile.org"
+    }
+)

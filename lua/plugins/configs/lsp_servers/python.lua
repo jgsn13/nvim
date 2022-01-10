16bffa50fc8f2427293deaa75e40d6f98792b9bb
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-10 16:46:46 -0300

    update: python lsp server

--- lua/plugins/configs/lsp_servers/python.lua
+++ lua/plugins/configs/lsp_servers/python.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lsp_servers.on_attach")
 local capabilities = require("plugins.configs.lsp_servers.capabilities")
 
 -- require "lspconfig".pyright.setup {}
-require "lspconfig".pylsp.setup {
+require "lspconfig".pyright.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     root_dir = require "lspconfig".util.root_pattern(".git") or vim.fn.getcwd(),

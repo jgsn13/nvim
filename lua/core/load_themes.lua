205c36d1d564233eef16625a6b133820c746d1ab
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-18 15:00:31 -0300

    update(colorscheme): doom-one

--- lua/core/load_themes.lua
+++ lua/core/load_themes.lua
@@ -1,25 +1,19 @@
 function load_themes()
-    -- require("doom-one").setup(
-    --     {
-    --         cursor_coloring = false,
-    --         terminal_colors = true,
-    --         italic_comments = true,
-    --         enable_treesitter = true,
-    --         transparent_background = false
-    --     }
-    -- )
+    require("doom-one").setup(
+        {
+            cursor_coloring = false,
+            terminal_colors = true,
+            italic_comments = true,
+            enable_treesitter = true,
+            transparent_background = false
+        }
+    )
 
     -- fix NvimTreeNormalNC when transparency is activated
     vim.cmd [[highlight NvimTreeNormalNC guibg=NONE]]
 
     -- vim.cmd[[colorscheme dracula]]
     -- require("moonlight").set()
-    require("onedark").setup {
-        style = "cool",
-        toggle_style_key = "<leader>ts", -- Default keybinding to toggle
-        toggle_style_list = {"dark", "darker", "cool", "deep", "warm", "warmer", "light"} -- List of styles to toggle between
-    }
-    require("onedark").load()
     -- require("calvera").set()
     -- require("github-theme").setup()
     -- require("catppuccin").setup()

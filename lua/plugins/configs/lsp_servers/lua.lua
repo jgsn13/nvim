aa6175d0b7c1cf381adc6445acdfce881d23cdcb
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-02-19 07:03:56 -0300

    fix(lsp): lua lsp path

--- lua/plugins/configs/lsp_servers/lua.lua
+++ lua/plugins/configs/lsp_servers/lua.lua
@@ -1,18 +1,7 @@
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has("win32") == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
-
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 --local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
-local sumneko_root_path = "/home/joaquim/LangServers/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+local sumneko_root_path = "/home/joaquim/LanguageServers/lua-language-server/bin"
+local sumneko_binary = sumneko_root_path .. "/lua-language-server"
 
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")

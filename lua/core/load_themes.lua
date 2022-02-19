b15aef56e8c282345cc9ff6d9f60954003d158fd
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-02-19 07:02:54 -0300

    remove all unused themes

--- lua/core/load_themes.lua
+++ lua/core/load_themes.lua
@@ -1,23 +1,27 @@
 function load_themes()
-    require("doom-one").setup(
+    local onedark = require("onedark")
+    onedark.setup(
         {
-            cursor_coloring = false,
-            terminal_colors = true,
-            italic_comments = true,
-            enable_treesitter = true,
-            transparent_background = false
+            styles = {
+                strings = "NONE", -- Style that is applied to strings
+                comments = "italic", -- Style that is applied to comments
+                keywords = "NONE", -- Style that is applied to keywords
+                functions = "bold,italic", -- Style that is applied to functions
+                variables = "NONE" -- Style that is applied to variables
+            },
+            options = {
+                bold = true, -- Use the themes opinionated bold styles?
+                italic = true, -- Use the themes opinionated italic styles?
+                underline = true, -- Use the themes opinionated underline styles?
+                undercurl = true, -- Use the themes opinionated undercurl styles?
+                cursorline = false, -- Use cursorline highlighting?
+                transparency = false, -- Use a transparent background?
+                terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
+                window_unfocussed_color = false -- When the window is out of focus, change the normal background?
+            }
         }
     )
-
-    -- fix NvimTreeNormalNC when transparency is activated
-    vim.cmd [[highlight NvimTreeNormalNC guibg=NONE]]
-
-    -- vim.cmd[[colorscheme dracula]]
-    -- require("moonlight").set()
-    -- require("calvera").set()
-    -- require("github-theme").setup()
-    -- require("catppuccin").setup()
-    -- require("onenord").setup()
+    onedark.load()
 end
 
 return load_themes()

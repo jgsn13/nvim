3de90558afd8c52e2f10efd1e2ad4af079701bec
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-06-26 15:23:49 -0300

    fix: remove 'autopairs_completion.setup' function

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -4,17 +4,17 @@ local config = require("core.utils").load_config()
 
 M.autopairs = function()
     local present1, autopairs = pcall(require, "nvim-autopairs")
-    local present2, autopairs_completion = pcall(require, "nvim-autopairs.completion.cmp")
+    -- local present2, autopairs_completion = pcall(require, "nvim-autopairs.completion.cmp")
 
     if not (present1 or present2) then
         return
     end
 
     autopairs.setup()
-    autopairs_completion.setup {
-        map_complete = true, -- insert () func completion
-        map_cr = true
-    }
+    -- autopairs_completion.setup {
+    --     map_complete = true, -- insert () func completion
+    --     map_cr = true
+    -- }
 end
 
 M.blankline = function()

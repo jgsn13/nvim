4083798ab391907f3d9679efae6011dbee7af6cd
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-30 11:07:15 -0300

    chore: setup a basic configuration for neovide

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 -- Don't change terminal cursor shape
-vim.cmd
-[[
+vim.cmd [[
     augroup change_cursor
         au!
         au ExitPre * :set guicursor=a:ver90
     augroup END
-]] 
+]]
 
 local init_modules = {
     "core"
@@ -17,3 +16,12 @@ for _, module in ipairs(init_modules) do
         error("Error loading " .. module .. "\n\n" .. err)
     end
 end
+
+vim.cmd [[
+if exists("g:neovide")
+    " Put anything you want to happen only in Neovide here
+    let g:neovide_transparency=0.88
+    set guifont=FiraCode\ NF:h8
+    let g:neovide_cursor_vfx_mode = "railgun"
+endif
+]]

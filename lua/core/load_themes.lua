c4fe8801f185203763631b4e2f52194ffce1baf7
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-09-03 21:13:56 -0300

    feat: add cursorline, transparency and window_unfocussed_color to default theme

--- lua/core/load_themes.lua
+++ lua/core/load_themes.lua
@@ -14,10 +14,10 @@ function load_themes()
                 italic = true, -- Use the themes opinionated italic styles?
                 underline = true, -- Use the themes opinionated underline styles?
                 undercurl = true, -- Use the themes opinionated undercurl styles?
-                cursorline = false, -- Use cursorline highlighting?
-                transparency = false, -- Use a transparent background?
+                cursorline = true, -- Use cursorline highlighting?
+                transparency = true, -- Use a transparent background?
                 terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
-                window_unfocussed_color = false -- When the window is out of focus, change the normal background?
+                window_unfocussed_color = true -- When the window is out of focus, change the normal background?
             }
         }
     )

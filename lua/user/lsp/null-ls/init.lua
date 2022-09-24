dcdd3ba73c513f593641f24fc627bd8908f6600c
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-09-24 15:27:45 -0300

    feat: add null-ls

--- lua/user/lsp/null-ls/init.lua
+++ lua/user/lsp/null-ls/init.lua
@@ -3,8 +3,8 @@ if not present then
 	return
 end
 
-local formatting = null_ls.builtins.formatting
-local formatting_settings = require("user.lsp.null-ls.formatting")
+-- local formatting = null_ls.builtins.formatting
+-- local formatting_settings = require("user.lsp.null-ls.formatting")
 
 local diagnostics = null_ls.builtins.diagnostics
 
@@ -12,15 +12,17 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		-- Formatting settings
-		formatting.prettier.with(formatting_settings.prettier),
-		formatting.rustfmt.with(formatting_settings.rustfmt),
-		formatting.stylua.with(formatting_settings.stylua),
-		formatting.clang_format.with(formatting_settings.clang_format),
-		formatting.black.with(formatting_settings.black),
+		-- formatting.prettier.with(formatting_settings.prettier),
+		-- formatting.rustfmt.with(formatting_settings.rustfmt),
+		-- formatting.stylua.with(formatting_settings.stylua),
+		-- formatting.clang_format.with(formatting_settings.clang_format),
+		-- formatting.black.with(formatting_settings.black),
+
 		-- DIagnostics
 		-- diagnostics.eslint,
 		diagnostics.flake8,
+		diagnostics.pylint,
 	},
 })
 
-KEYMAP("n", "<Leader>f", ":Format<cr>", OPTS)
+-- KEYMAP("n", "<Leader>f", ":Format<cr>", OPTS)

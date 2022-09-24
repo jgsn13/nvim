b0b2233f70f888e8095244184eea4b5ea41f858f
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-09-24 13:49:13 -0300

    feat: remove null-ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,7 +72,7 @@ return packer.startup(function(use)
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	-- use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 	use("j-hui/fidget.nvim")
 
 	-- Telescope

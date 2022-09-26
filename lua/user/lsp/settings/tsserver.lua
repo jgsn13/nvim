64037daa3c9287403bc26cec4ca557ad387157dc
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-09-26 08:13:04 -0300

    feat: setup tsserver manually

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,13 @@
+return {
+	flags = {
+		debounce_text_changes = 150,
+	},
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+}

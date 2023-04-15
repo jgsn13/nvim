a0e719fda8c1cadeb3ddfbc5078693d0abab1d35
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2023-04-15 14:26:22 -0300

    fix: show hidden files on neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -200,9 +200,9 @@ return {
 			filesystem = {
 				filtered_items = {
 					visible = false, -- when true, they will just be displayed differently than normal items
-					hide_dotfiles = true,
-					hide_gitignored = true,
-					hide_hidden = true, -- only works on Windows for hidden files/directories
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false, -- only works on Windows for hidden files/directories
 					hide_by_name = {
 						--"node_modules"
 					},

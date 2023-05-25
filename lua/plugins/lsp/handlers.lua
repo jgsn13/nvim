ad0888691e1d521cf683e3bc1e5c54ad4ec465f5
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2023-05-25 08:17:53 -0300

    fix(keymap): remove 'leader + e' conflict

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -56,7 +56,7 @@ local function lsp_keymaps(bufnr)
 	vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-	vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<space>d", vim.diagnostic.open_float, opts)
 	vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
